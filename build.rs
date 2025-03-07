9fb26aabe89858e03abf52c1a3b56e9ccf4cfc9c
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-03-07 10:28:40 +0100

    Add wrapper and build for C bindings

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to re-run if the wrapper.h changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // Link to the p40 libraries
+    println!("cargo:rustc-link-lib=p40");
+    println!("cargo:rustc-link-lib=p40_daq");
+
+    // Generate bindings using the wrapper header
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,4 @@
+#include <lhcb/pcie40/daq.h>
+#include <lhcb/pcie40/id.h>
+#include <lhcb/pcie40/stream.h>
+#include <lhcb/pcie40/ctrl.h>
