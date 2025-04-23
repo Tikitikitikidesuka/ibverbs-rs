2f9d2897d10b5d66d25627fa938ec66f5f9419a6
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-04-23 15:29:20 +0200

    Put all non essentials in features

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ extern crate core;
 pub mod typed_zero_copy_ring_buffer_reader;
 pub mod zero_copy_ring_buffer_reader;
 
-#[cfg(feature = "multi_fragment_packet")]
+#[cfg(feature = "multi-fragment-packet")]
 pub mod multi_fragment_packet;
 
 #[cfg(feature = "pcie40")]
 pub mod pcie40;
 
-pub mod utils;
-
+#[cfg(feature = "mock-reader")]
 pub mod mock_reader;
+
+#[cfg(feature = "utils")]
+pub mod utils;
