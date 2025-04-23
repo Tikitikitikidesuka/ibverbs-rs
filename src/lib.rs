bd2bfa1f93cc5c92d5266d0c238bbdff76a9b8cb
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-04-23 12:39:28 +0200

    Separate by features and move main to an example

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,10 @@ mod bindings {
 pub mod typed_zero_copy_ring_buffer_reader;
 pub mod zero_copy_ring_buffer_reader;
 
+#[cfg(feature = "multi_fragment_packet")]
 pub mod multi_fragment_packet;
+
+#[cfg(feature = "pcie40")]
 pub mod pcie40;
 
 pub mod utils;
