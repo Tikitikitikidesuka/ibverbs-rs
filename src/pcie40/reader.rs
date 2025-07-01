0c5aed3f57ee21060b6fb32acf38f68ca08e8c8e
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-07-01 15:59:45 +0200

    Update pcie40 typo on method call 2pow

--- src/pcie40/reader.rs
+++ src/pcie40/reader.rs
@@ -7,7 +7,7 @@ use thiserror::Error;
 pub struct PCIe40Reader<'a> {
     mapped_buffer: PCIe40MappedStream<'a>,
     read_offset: usize,
-    alignment_2pow: u8,
+    alignment_pow2: u8,
 }
 
 #[derive(Debug, Error)]
@@ -41,35 +41,35 @@ pub enum PCIe40AdvanceError {
 impl<'a> PCIe40Reader<'a> {
     pub fn new(
         mapped_buffer: PCIe40MappedStream<'a>,
-        alignment_2pow: u8,
+        alignment_pow2: u8,
     ) -> Result<Self, PCIe40ReaderInstanceError> {
         // Size has to be aligned for advance alignment check to work properly
         let buffer_size = unsafe { mapped_buffer.data().len() };
-        if !utils::check_alignment_pow2(buffer_size, alignment_2pow) {
+        if !utils::check_alignment_pow2(buffer_size, alignment_pow2) {
             return Err(PCIe40ReaderInstanceError::BufferSizeNotAligned {
                 buffer_size,
-                alignment: utils::pow2(alignment_2pow),
+                alignment: utils::pow2(alignment_pow2),
             });
         }
 
         // Read offset has to be aligned for advance alignment check to work properly
         let read_offset = mapped_buffer.get_read_offset()?;
-        if !utils::check_alignment_pow2(read_offset, alignment_2pow) {
+        if !utils::check_alignment_pow2(read_offset, alignment_pow2) {
             return Err(PCIe40ReaderInstanceError::ReadOffsetNotAligned {
                 read_offset,
-                alignment: utils::pow2(alignment_2pow),
+                alignment: utils::pow2(alignment_pow2),
             });
         }
 
         Ok(Self {
             mapped_buffer,
             read_offset,
-            alignment_2pow,
+            alignment_pow2,
         })
     }
 
     pub fn alignment_pow2(&self) -> u8 {
-        self.alignment_2pow
+        self.alignment_pow2
     }
 }
 
@@ -78,7 +78,7 @@ impl<'r> CircularBufferReader for PCIe40Reader<'r> {
     type ReadableRegionResult<'a> = Result<&'a [u8], PCIe40StreamError> where Self: 'a, 'r: 'a;
 
     fn advance_read_pointer(&mut self, bytes: usize) -> Self::AdvanceResult {
-        if !utils::check_alignment_pow2(self.read_offset + bytes, self.alignment_2pow) {
+        if !utils::check_alignment_pow2(self.read_offset + bytes, self.alignment_pow2) {
             return Err(PCIe40AdvanceError::NotAligned);
         }
 
@@ -88,7 +88,7 @@ impl<'r> CircularBufferReader for PCIe40Reader<'r> {
             .map_err(|_| PCIe40AdvanceError::OutOfBounds)?;
 
         // Update local read offset wrapping to fit the buffer boundary
-        self.read_offset = utils::wrap_around_pow2(self.read_offset + bytes, self.alignment_2pow);
+        self.read_offset = utils::wrap_around_pow2(self.read_offset + bytes, self.alignment_pow2);
 
         Ok(())
     }
