3009db59e99257c25e23d106b68504567467616d
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-07-01 15:59:20 +0200

    Add mock to lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate core;
 
 pub mod circular_buffer;
 pub mod typed_circular_buffer;
+pub mod typed_circular_buffer_read_guard;
 
 #[cfg(feature = "multi-fragment-packet")]
 pub mod multi_fragment_packet;
@@ -9,12 +10,11 @@ pub mod multi_fragment_packet;
 #[cfg(feature = "pcie40")]
 pub mod pcie40;
 
-#[cfg(feature = "mock-reader")]
-pub mod mock_reader;
+#[cfg(feature = "shared-memory")]
+pub mod shared_memory_buffer;
 
 #[cfg(feature = "utils")]
 pub mod utils;
 
-#[cfg(feature = "shared-memory")]
-pub mod shared_memory_buffer;
-mod typed_circular_buffer_read_guard;
+#[cfg(feature = "mock-buffers")]
+pub mod mock_buffers;
