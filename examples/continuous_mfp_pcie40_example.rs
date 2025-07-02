2150193361d228534c474ca5730aa3c09e45d61f
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-07-02 17:14:47 +0200

    Add continuous reading mfp example for pcie40

---
+++ examples/continuous_mfp_pcie40_example.rs
@@ -0,0 +1,58 @@
+use pcie40_rs::pcie40::ctrl::PCIe40ControllerManager;
+use pcie40_rs::pcie40::reader::PCIe40Reader;
+use pcie40_rs::pcie40::stream::stream::PCIe40DAQStreamFormat::MetaFormat;
+use pcie40_rs::pcie40::stream::stream::PCIe40DAQStreamType::MainStream;
+use pcie40_rs::pcie40::stream::stream::PCIe40StreamHandleEnableStateCloseMode::PreserveEnableState;
+use pcie40_rs::pcie40::stream::stream::PCIe40StreamManager;
+use pcie40_rs::utils;
+use pcie40_rs::utils::IsPow2Result;
+use std::io::{Read, stdin};
+use pcie40_rs::multi_fragment_packet::MultiFragmentPacketRef;
+use pcie40_rs::typed_circular_buffer::CircularBufferMultiReadable;
+
+fn main() {
+    const DEVICE_NAME: &str = "tdtel203_1";
+
+    let controller = PCIe40ControllerManager::open_by_device_name(DEVICE_NAME).unwrap();
+    let meta_alignment_pow2 = match utils::is_pow2(controller.meta_alignment().unwrap()) {
+        IsPow2Result::Yes(pow2) => pow2,
+        IsPow2Result::No => {
+            panic!("Meta alignment is not a power of 2")
+        }
+    };
+
+    let mut stream =
+        PCIe40StreamManager::open_by_device_name(DEVICE_NAME, MainStream, MetaFormat).unwrap();
+    stream
+        .set_raii_enable_state_close_mode(PreserveEnableState)
+        .unwrap();
+
+    let mut locked_stream = stream.lock().unwrap();
+    locked_stream.flush().unwrap();
+
+    let mapped_stream = locked_stream.map_buffer().unwrap();
+
+    let mut reader = PCIe40Reader::new(mapped_stream, meta_alignment_pow2).unwrap();
+
+    println!("\n\nStream configured... Press any key to proceed\n");
+    stdin().read_exact(&mut [0]).unwrap();
+
+    loop {
+        println!("Loading 5 MFPs...");
+        match MultiFragmentPacketRef::read_multiple(&mut reader, 2) {
+            Ok(mfps) => {
+                println!("Read MFP[0]: {:?}", mfps[0]);
+                println!("Read MFP[1]: {:?}", mfps[1]);
+                println!("Read MFP[2]: {:?}", mfps[2]);
+                println!("Read MFP[3]: {:?}", mfps[3]);
+                println!("Read MFP[4]: {:?}", mfps[4]);
+                println!("Discarding MFPs...");
+                mfps.discard().expect("Error discarding");
+                println!("...\n");
+            }
+            Err(_) => {println!("Waiting for MFPs...")}
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+}
