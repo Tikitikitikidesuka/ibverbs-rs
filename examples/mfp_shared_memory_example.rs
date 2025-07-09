0790921d4a68a2c0d92780924b36dcd4f24f4e54
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-07-09 15:15:43 +0200

    Add better print guides to shmem example mfp

--- examples/mfp_shared_memory_example.rs
+++ examples/mfp_shared_memory_example.rs
@@ -18,7 +18,10 @@ fn main() {
     let mut reader = SharedMemoryBufferReader::new(read_buffer);
     let mut writer = SharedMemoryBufferWriter::new(write_buffer);
 
+    println!("Ready!!!");
+
     // [0, , , ]
+    println!("Writing MFP 0 to shmem...");
     let mfp_0_256 = MultiFragmentPacketBuilder::new()
         .with_align(4)
         .with_event_id(0)
@@ -29,18 +32,23 @@ fn main() {
         .build();
     mfp_0_256.write(&mut writer).unwrap();
     println!(
-        "Size: {}",
+        "Done! Size on buffer: {}",
         utils::align_up_pow2(mfp_0_256.packet_size() as usize, writer.alignment_pow2())
     );
 
     // [0,1, , ]
     // Writable is also implemented for the buffered entry so one can be
     // read and written again without copying it out of the buffer
+    println!("Writing MFP 0 again to shmem...");
     let read_mfp = MultiFragmentPacketRef::read(&mut reader).unwrap();
     read_mfp.write(&mut writer).unwrap();
-    println!("Same one");
+    println!(
+        "Done! Size on buffer: {}",
+        utils::align_up_pow2(read_mfp.packet_size() as usize, writer.alignment_pow2())
+    );
 
     // [0,1,2, ]
+    println!("Writing MFP 2 to shmem...");
     let mfp_2_256 = MultiFragmentPacketBuilder::new()
         .with_align(4)
         .with_event_id(2)
@@ -51,21 +59,26 @@ fn main() {
         .build();
     mfp_2_256.write(&mut writer).unwrap();
     println!(
-        "Size: {}",
+        "Done! Size on buffer: {}",
         utils::align_up_pow2(mfp_2_256.packet_size() as usize, writer.alignment_pow2())
     );
 
     // [ ,1,2, ]
+    println!("Reading first instance of MFP 0 from shmem...");
     let read_mfp = MultiFragmentPacketRef::read(&mut reader).unwrap();
-    println!("Consume: {}", *read_mfp);
+    println!("Read: {}", *read_mfp);
+    println!("Discarding it...");
     read_mfp.discard().unwrap();
 
     // [ , ,2, ]
-    let read_entry = MultiFragmentPacketRef::read(&mut reader).unwrap();
-    println!("Consume: {}", *read_entry);
-    read_entry.discard().unwrap();
+    println!("Reading second instance of MFP 0 from shmem...");
+    let read_mfp = MultiFragmentPacketRef::read(&mut reader).unwrap();
+    println!("Read: {}", *read_mfp);
+    println!("Discarding it...");
+    read_mfp.discard().unwrap();
 
     // [3,3,2,W]
+    println!("Writing MFP 3 to shmem (this one sholuld trigger wrap behaviour)...");
     let mfp_3_512 = MultiFragmentPacketBuilder::new()
         .with_align(4)
         .with_event_id(3)
@@ -76,14 +89,16 @@ fn main() {
         .build();
     mfp_3_512.write(&mut writer).unwrap();
     println!(
-        "Size: {}",
+        "Done! Size on buffer: {}",
         utils::align_up_pow2(mfp_3_512.packet_size() as usize, writer.alignment_pow2())
     );
 
     // [ , , , ]
+    println!("Reading MFPs 0 and 3 from shmem...");
     let read_entries = MultiFragmentPacketRef::read_multiple(&mut reader, 2).unwrap();
     read_entries.iter().for_each(|entry| {
         println!("Read many: {}", entry);
     });
+    println!("Discarding them...");
     read_entries.discard().unwrap();
 }
