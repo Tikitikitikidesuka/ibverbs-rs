5f9de996b04f9fa90b36d9433dca574da2182760
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-07-31 14:45:55 +0200

    Working send receive ib

--- crates/infinibuilder/bin/loopback.rs
+++ crates/infinibuilder/bin/loopback.rs
@@ -1,27 +1,55 @@
-use ibverbs::ibv_access_flags;
-use ibverbs::ibv_qp_type::IBV_QPT_RC;
-
 fn main() {
-    let context = ibverbs::devices()
+    let ctx = ibverbs::devices()
         .unwrap()
         .iter()
         .next()
-        .unwrap()
+        .expect("no rdma device available")
         .open()
         .unwrap();
 
-    let cq = context.create_cq(16, 0).unwrap();
-    let pd = context.alloc_pd().unwrap();
+    let cq = ctx.create_cq(16, 0).unwrap();
+    let pd = ctx.alloc_pd().unwrap();
 
-    let qp_builder = pd.create_qp(&cq, &cq, IBV_QPT_RC)
+    let qp_builder = pd
+        .create_qp(&cq, &cq, ibverbs::ibv_qp_type::IBV_QPT_RC)
         .unwrap()
-        //.set_gid_index(1)
+        .set_gid_index(1)
         .build()
         .unwrap();
 
     let endpoint = qp_builder.endpoint().unwrap();
     let mut qp = qp_builder.handshake(endpoint).unwrap();
 
-    let mut mr = pd.allocate(256).unwrap();
-    let mem = mr.inner().as_mut_slice();
+    let mut mr = pd.allocate(16).unwrap();
+    mr.inner()[9] = 0x42;
+
+    unsafe { qp.post_receive(&[mr.slice(..8)], 2) }.unwrap();
+    unsafe { qp.post_send(&[mr.slice(8..)], 1) }.unwrap();
+
+    let mut sent = false;
+    let mut received = false;
+    let mut completions = [ibverbs::ibv_wc::default(); 16];
+    while !sent || !received {
+        let completed = cq.poll(&mut completions[..]).unwrap();
+        if completed.is_empty() {
+            continue;
+        }
+        assert!(completed.len() <= 2);
+        for wr in completed {
+            match wr.wr_id() {
+                1 => {
+                    assert!(!sent);
+                    sent = true;
+                    println!("sent");
+                }
+                2 => {
+                    assert!(!received);
+                    received = true;
+                    assert_eq!(mr.inner()[1], 0x42);
+                    println!("received");
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
--- crates/infinibuilder/bin/test.rs
+++ crates/infinibuilder/bin/test.rs
@@ -1,19 +1,61 @@
-use infinibuilder::EndpointBuilder;
+use infinibuilder::IbBEndpointBuilder;
+use std::ptr::slice_from_raw_parts_mut;
 
 fn main() {
+    println!("Listing devices...");
+
     let devices = ibverbs::devices().unwrap();
-    let context = devices.get(0).unwrap().open().unwrap();
 
-    let mut memory_a = [0u8; 1024];
-    let mut memory_b = [0u8; 1024];
+    println!(
+        "Devices: {:?}",
+        devices.iter().map(|d| d.name()).collect::<Vec<_>>()
+    );
 
-    let builder_a = EndpointBuilder::new(&context).set_data_memory_region(&mut memory_a);
-    let builder_b = EndpointBuilder::new(&context).set_data_memory_region(&mut memory_b);
-    /*
-    let endpoint_a = builder_a.endpoint();
-    let endpoint_b = builder_b.endpoint();
-    builder_a.connect(endpoint_b);
-    builder_b.connect(endpoint_a);
+    println!("Opening contexts...");
+    let context_a = devices.get(0).unwrap().open().unwrap();
+    let context_b = devices.get(1).unwrap().open().unwrap();
 
-     */
+    println!("Creating memories...");
+    let mut memory_a = [0u8, 0, 0, 0];
+    let mut memory_b = [0u8, 1, 2, 3];
+
+    println!("Decoupling memory unsafely for independent mutable reference");
+    let rdma_mem_a = unsafe { &mut *(&mut memory_a[..] as *mut [u8]) };
+    let rdma_mem_b = unsafe { &mut *(&mut memory_b[..] as *mut [u8]) };
+
+    println!("Creating endpoints...");
+    let endpoint_a = IbBEndpointBuilder::new()
+        .set_context(&context_a)
+        .set_data_memory_region(rdma_mem_a)
+        .set_completion_queue_size(16)
+        .build();
+    let endpoint_b = IbBEndpointBuilder::new()
+        .set_context(&context_b)
+        .set_data_memory_region(rdma_mem_b)
+        .set_completion_queue_size(16)
+        .build();
+
+    println!("Endpoint A: {:?}", endpoint_a.endpoint());
+    println!("Endpoint B: {:?}", endpoint_b.endpoint());
+
+    println!("Establishing connections...");
+    let mut endpoint_a = endpoint_a.connect(endpoint_b.endpoint());
+    let mut endpoint_b = endpoint_b.connect(endpoint_a.endpoint());
+
+    println!("Connection established");
+
+    println!("Memory A: {:?}", memory_a);
+    println!("Memory B: {:?}", memory_b);
+
+    let receive_wr = endpoint_a.post_receive(..);
+    let extra_receive_wr = endpoint_a.post_receive(..);
+    let send_wr = endpoint_b.post_send(1..2);
+
+    println!("Waiting for receive wr...");
+    receive_wr.wait();
+
+    println!("Memory A: {:?}", memory_a);
+    println!("Memory B: {:?}", memory_b);
+
+    //extra_receive_wr.wait();
 }
