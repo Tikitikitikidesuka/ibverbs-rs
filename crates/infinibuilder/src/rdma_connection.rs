bea96fca49875ee0df08d378c49c4747437ceadb
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2025-11-03 10:05:02 +0100

    Update rdma work request spin poll

--- crates/infinibuilder/src/rdma_connection.rs
+++ crates/infinibuilder/src/rdma_connection.rs
@@ -1,4 +1,4 @@
-use crate::spin_poll::{Timeout, spin_poll, spin_poll_batched};
+use crate::spin_poll::{spin_poll_timeout, spin_poll_timeout_batched};
 use std::error::Error;
 use std::ops::RangeBounds;
 use std::time::Duration;
@@ -13,12 +13,11 @@ pub trait RdmaConnection<MR, RMR>:
 }
 
 // Blanket implementation
-impl<MR, RMR, T> RdmaConnection<MR, RMR> for T
-where
+impl<MR, RMR, T> RdmaConnection<MR, RMR> for T where
     T: RdmaNamedMemoryRegionConnection<MR, RMR>
-    + RdmaSendReceiveConnection<MR>
-    + RdmaReadWriteConnection<MR, RMR>
-    + RdmaImmediateDataConnection,
+        + RdmaSendReceiveConnection<MR>
+        + RdmaReadWriteConnection<MR, RMR>
+        + RdmaImmediateDataConnection
 {
 }
 
@@ -31,7 +30,6 @@ pub trait RdmaSendReceiveConnection<MR> {
     type PostError: Error;
     type WR: RdmaWorkRequest;
 
-    // Posts a send operation. Will fail if the remote has not posted a receive operation before hand.
     fn post_send(
         &mut self,
         memory_region: &MR,
@@ -39,7 +37,6 @@ pub trait RdmaSendReceiveConnection<MR> {
         immediate_data: Option<u32>,
     ) -> Result<Self::WR, Self::PostError>;
 
-    // Posts a receive operation.
     fn post_receive(
         &mut self,
         memory_region: &MR,
@@ -51,9 +48,6 @@ pub trait RdmaReadWriteConnection<MR, RMR> {
     type PostError: Error;
     type WR: RdmaWorkRequest;
 
-    // Posts a write operation.
-    // If sent with immediate data, the data must be obtained in the remote peer
-    // by calling post_receive_immediate
     fn post_write(
         &mut self,
         local_memory_region: &MR,
@@ -63,7 +57,6 @@ pub trait RdmaReadWriteConnection<MR, RMR> {
         immediate_data: Option<u32>,
     ) -> Result<Self::WR, Self::PostError>;
 
-    // Posts a read operation.
     fn post_read(
         &mut self,
         local_memory_region: &MR,
@@ -102,19 +95,18 @@ pub trait RdmaWorkRequest {
         timeout: Duration,
     ) -> Result<(Self::WC, Duration), WorkRequestSpinPollError<Self::PollError, Self::RdmaError>>
     {
-        match spin_poll(
+        spin_poll_timeout(
             || match self.poll() {
-                RdmaWorkRequestStatus::Pending => None,
-                RdmaWorkRequestStatus::Success(wc) => Some(Ok(wc)),
-                RdmaWorkRequestStatus::Error(err) => Some(Err(err)),
+                RdmaWorkRequestStatus::Pending => Err(None),
+                RdmaWorkRequestStatus::Success(wc) => Ok(wc),
+                RdmaWorkRequestStatus::Error(err) => Err(Some(err)),
             },
             timeout,
-        ) {
-            Ok((inner, dur)) => inner
-                .map(|wc| (wc, dur))
-                .map_err(|error| WorkRequestSpinPollError::ExecutionError(error)),
-            Err(timeout) => Err(WorkRequestSpinPollError::Timeout(timeout)),
-        }
+        )
+        .map_err(|error| match error {
+            None => WorkRequestSpinPollError::Timeout,
+            Some(error) => WorkRequestSpinPollError::ExecutionError(error),
+        })
     }
 
     fn spin_poll_batched(
@@ -123,20 +115,19 @@ pub trait RdmaWorkRequest {
         batch_iters: usize,
     ) -> Result<(Self::WC, Duration), WorkRequestSpinPollError<Self::PollError, Self::RdmaError>>
     {
-        match spin_poll_batched(
+        spin_poll_timeout_batched(
             || match self.poll() {
-                RdmaWorkRequestStatus::Pending => None,
-                RdmaWorkRequestStatus::Success(wc) => Some(Ok(wc)),
-                RdmaWorkRequestStatus::Error(err) => Some(Err(err)),
+                RdmaWorkRequestStatus::Pending => Err(None),
+                RdmaWorkRequestStatus::Success(wc) => Ok(wc),
+                RdmaWorkRequestStatus::Error(err) => Err(Some(err)),
             },
             timeout,
             batch_iters,
-        ) {
-            Ok((inner, dur)) => inner
-                .map(|wc| (wc, dur))
-                .map_err(|error| WorkRequestSpinPollError::ExecutionError(error)),
-            Err(timeout) => Err(WorkRequestSpinPollError::Timeout(timeout)),
-        }
+        )
+        .map_err(|error| match error {
+            None => WorkRequestSpinPollError::Timeout,
+            Some(error) => WorkRequestSpinPollError::ExecutionError(error),
+        })
     }
 }
 
@@ -150,8 +141,8 @@ pub enum WorkRequestPollError<PollError: Error, RdmaError: Error> {
 
 #[derive(Debug, Error)]
 pub enum WorkRequestSpinPollError<PollError: Error, RdmaError: Error> {
-    #[error("Work request spin poll timeout: {0}")]
-    Timeout(Timeout),
+    #[error("Work request spin poll timeout")]
+    Timeout,
     #[error("Work request spin poll error: {0}")]
     ExecutionError(#[from] WorkRequestPollError<PollError, RdmaError>),
 }
