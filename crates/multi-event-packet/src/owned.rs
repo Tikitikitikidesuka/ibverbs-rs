f42c511e4dde082a0e7134ded37796a428d99ac3
Jonatan Ziegler <jonatan.ziegler@cern.ch>
2025-12-11 10:39:12 +0100

    mep-reader

--- crates/multi-event-packet/src/owned.rs
+++ crates/multi-event-packet/src/owned.rs
@@ -8,11 +8,11 @@ use crate::{MultiEventPacket, builder::MultiEventPacketBuilder};
 ///
 /// An owned MEP can be constructed using the [`MultiEventPacketBuilder`].
 #[derive(Clone)]
-pub struct MultiEventPacketOwned {
-    data: Box<[u32]>, // assures alignement of u32
+pub struct MultiEventPacketOwned<Data: AsRef<[u32]> = Box<[u32]>> {
+    data: Data, // assures alignment of u32
 }
 
-impl Deref for MultiEventPacketOwned {
+impl<D: AsRef<[u32]>> Deref for MultiEventPacketOwned<D> {
     type Target = MultiEventPacket;
 
     fn deref(&self) -> &Self::Target {
@@ -20,15 +20,16 @@ impl Deref for MultiEventPacketOwned {
     }
 }
 
-impl AsRef<MultiEventPacket> for MultiEventPacketOwned {
+impl<D: AsRef<[u32]>> AsRef<MultiEventPacket> for MultiEventPacketOwned<D> {
     fn as_ref(&self) -> &MultiEventPacket {
         // MultiEventPacket must be guaranteed to be correct already. Since it can only
         // be built by the builder, it is supposed to be guaranteed.
-        unsafe { MultiEventPacket::unchecked_from_raw_bytes(&self.data) }
+        // todo enforce this...
+        unsafe { MultiEventPacket::unchecked_from_raw_bytes(self.data.as_ref()) }
     }
 }
 
-impl Borrow<MultiEventPacket> for MultiEventPacketOwned {
+impl<D: AsRef<[u32]>> Borrow<MultiEventPacket> for MultiEventPacketOwned<D> {
     fn borrow(&self) -> &MultiEventPacket {
         self
     }
@@ -39,11 +40,51 @@ impl MultiEventPacketOwned {
     pub fn builder<'a>() -> MultiEventPacketBuilder<'a> {
         MultiEventPacketBuilder::new()
     }
+}
 
+impl<D: AsRef<[u32]>> MultiEventPacketOwned<D> {
     /// Creates a new owned MEP from a boxed slice of `u32`.
     /// # Safety
     /// Data needs to be a valid [`MultiEventPacket`].
-    pub unsafe fn from_data(data: Box<[u32]>) -> Self {
+    pub unsafe fn from_data(data: D) -> Self {
         Self { data }
+    }
+}
+
+#[cfg(feature = "mmap")]
+pub mod mmap {
+    use std::{fs::File, path::Path};
+
+    use super::*;
+
+    use memmap2::Mmap;
+
+    /// Wrapper struct for a `u32` aligned memory mapped region.
+    ///
+    /// Expects memory mapped files to be page aligned, and pages to be larger than `u32` 😉.
+    pub struct MemMap(Mmap);
+
+    impl AsRef<[u32]> for MemMap {
+        fn as_ref(&self) -> &[u32] {
+            bytemuck::try_cast_slice(self.0.as_ref()).expect("alignment matches, length compatible")
+        }
+    }
+
+    impl MultiEventPacketOwned<MemMap> {
+        /// Creates a new `MdfFile` by memory mapping a file at the given path.
+        ///
+        /// The advantage of this over using [`MdfFile::read_file`] is that the file
+        /// must not be read into memory at once but only as needed.
+        pub fn mmap_file(file: impl AsRef<Path>) -> std::io::Result<Self> {
+            let file = File::open(file)?;
+            let map = unsafe { Mmap::map(&file) }?;
+            #[cfg(unix)]
+            {
+                map.advise(memmap2::Advice::Sequential);
+            }
+
+            // todo enforce valid mep...
+            Ok(unsafe { Self::from_data(MemMap(map)) })
+        }
     }
 }
