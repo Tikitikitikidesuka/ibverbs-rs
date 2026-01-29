6f6323f81a34200dab4d6bd255369baeb28668bf
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2026-01-29 14:46:50 +0100

    Many barriers with random interval visualizer

---
+++ crates/infiniband-rs/examples/network_barrier_test_random.rs
@@ -0,0 +1,68 @@
+use infiniband_rs::ibverbs::devices::open_device;
+use infiniband_rs::ibverbs::work_request::{ReceiveWorkRequest, SendWorkRequest};
+use infiniband_rs::network::Node;
+use infiniband_rs::network::config::{NodeConfig, RawNetworkConfig};
+use infiniband_rs::network::tcp_exchanger::{ExchangeConfig, Exchanger};
+use log::LevelFilter::Debug;
+use rand::Rng;
+use simple_logger::SimpleLogger;
+use std::io::Read;
+use std::time::Duration;
+use std::{env, fs, process, thread};
+
+fn main() {
+    SimpleLogger::new().with_level(Debug).init().unwrap();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <network.json> <rank>", args[0]);
+        process::exit(1);
+    }
+
+    let network_path = &args[1];
+    let rank: usize = args[2].parse().unwrap();
+
+    let json_network = fs::read_to_string(network_path).unwrap();
+
+    let network_config = serde_json::from_str::<RawNetworkConfig>(&json_network)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let node_config: &NodeConfig = network_config.get(rank).unwrap();
+
+    let ctx = open_device(&node_config.ibdev).unwrap();
+
+    let node = Node::builder()
+        .context(&ctx)
+        .rank(node_config.rankid)
+        .world_size(network_config.world_size())
+        .build()
+        .unwrap();
+
+    let endpoint = node.endpoint();
+
+    let remote_endpoints =
+        Exchanger::await_exchange_all(rank, &network_config, &endpoint, &ExchangeConfig::default())
+            .unwrap();
+
+    let remote_endpoints = node.gather_endpoints(remote_endpoints).unwrap();
+
+    let mut node = node.handshake(remote_endpoints).unwrap();
+
+    let mut rng = rand::rng();
+    let all_nodes: Vec<_> = (0..node.world_size()).collect();
+
+    println!("Press a key...");
+    std::io::stdin().read(&mut []).expect("Failed to read line");
+
+    for i in 0..10000 {
+        let delay_ms = rng.random_range(10..10000000); // e.g., 10-1000ns
+        thread::sleep(Duration::from_nanos(delay_ms));
+
+        node.barrier(all_nodes.as_slice(), Duration::from_millis(10000))
+            .unwrap();
+        println!("Waited barrier {i} successfully");
+    }
+}
