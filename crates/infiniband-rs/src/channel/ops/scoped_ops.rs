ae8df9f5f2e7208e2c288d9d47ba9d6a071e95e5
Miguel Hermoso Mantecon <miguel.hermoso.mantecon@cern.ch>
2026-02-22 19:29:06 +0100

    Reorganize channel ops

--- crates/infiniband-rs/src/channel/ops/scoped_ops.rs
+++ crates/infiniband-rs/src/channel/ops/scoped_ops.rs
@@ -4,22 +4,6 @@ use crate::ibverbs::work::{
     ReadWorkRequest, ReceiveWorkRequest, SendWorkRequest, WriteWorkRequest,
 };
 
-impl Channel {
-    pub fn scope<'env, F, T, E>(&'env mut self, f: F) -> Result<T, ScopeError<E>>
-    where
-        F: for<'scope> FnOnce(&mut PollingScope<'scope, 'env, Channel>) -> Result<T, E>,
-    {
-        PollingScope::run(self, f)
-    }
-
-    pub fn manual_scope<'env, F, T, E>(&'env mut self, f: F) -> Result<T, E>
-    where
-        F: for<'scope> FnOnce(&mut PollingScope<'scope, 'env, Channel>) -> Result<T, E>,
-    {
-        PollingScope::run_manual(self, f)
-    }
-}
-
 impl<'scope, 'env> PollingScope<'scope, 'env, Channel> {
     pub fn post_send(
         &mut self,
--- crates/infiniband-rs/src/channel/polling_scope.rs
+++ crates/infiniband-rs/src/channel/polling_scope.rs
@@ -10,6 +10,22 @@ use std::panic::{AssertUnwindSafe, catch_unwind, resume_unwind};
 use std::rc::Rc;
 use thiserror::Error;
 
+impl Channel {
+    pub fn scope<'env, F, T, E>(&'env mut self, f: F) -> Result<T, ScopeError<E>>
+    where
+        F: for<'scope> FnOnce(&mut PollingScope<'scope, 'env, Channel>) -> Result<T, E>,
+    {
+        PollingScope::run(self, f)
+    }
+
+    pub fn manual_scope<'env, F, T, E>(&'env mut self, f: F) -> Result<T, E>
+    where
+        F: for<'scope> FnOnce(&mut PollingScope<'scope, 'env, Channel>) -> Result<T, E>,
+    {
+        PollingScope::run_manual(self, f)
+    }
+}
+
 /// T is user closure Ok output type
 /// E is user closure Err output type
 /// If user closure returns Err(E) -> auto poll -> return E
